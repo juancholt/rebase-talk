1b63ed13cc4232472bf80d90d0c3af7cd9bda6c7
Juan Hincapie <juan.hincapie.is@gmail.com>
2021-11-09 23:58:16 +0100

    feat: add token pages and api

--- pages/api/tokens/index.js
+++ pages/api/tokens/index.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import data from '../../../data/user.json';
+import data from '../../../data/tokens.json';
 
 export default function handler(req, res) {
   res.status(200).json(data)
